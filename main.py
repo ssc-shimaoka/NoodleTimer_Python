c53475dfe468e94128e5321a459881d61fb4b1f2
yuki shimaoka_home <yuki_shimaoka@solver.co.jp>
2020-10-15 21:23:15 +0900

    ・startTime設定追加

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from microbit import *
-import ButtonEvent
 import dispTime
 import TwoLineNumber
 
@@ -10,17 +9,13 @@ startTime = 0     #開始時間
 setTime = 0
 operatingTime = 0 #稼動時間
 elapsedTime = 0   #経過時間
-blockNumber = 25
+blockNumber = 2
 blockSurvivalTime = 30
 updateInterval = 1000   #更新間隔（ミリ秒）
 
 ###--------------------------------------
 ### タイマー処理
 def GetRemainingBlocks():
-    #global startTime
-    #global setTime
-    #global updateInterval
-    #global blockSurvivalTime
 
     #1秒間隔に調整
     sleep(updateInterval)
@@ -35,36 +30,64 @@ def GetRemainingBlocks():
 
 
 
-###
-
+###--------------------------------------
+#ループ処理
 while True:
-    ### Timerモード(通常モード)処理
+    #ボタンA＋Bイベント
+    if button_a.is_pressed() and button_b.is_pressed():
+        display.show(Image.BUTTERFLY)#仮
 
-    #共通処理
-    #残りブロック数取得
-    blocks = GetRemainingBlocks()
+    #ボタンAイベント
+    elif button_a.is_pressed():
+        #タイマー未動作状態
+        if appStatus == 0 :
+            #タイマー開始時間に現在の時刻を代入（ミリ秒）
+            startTime = running_time()
+            #タイマー指定時間　設定
+            setTime = blockNumber * blockSurvivalTime * updateInterval
+            #タイマー状態更新（未動作→動作中）
+            appStatus = 1
+            #出力
+            display.scroll("start")
+
+    #ボタンBイベント
+    elif button_b.is_pressed():
+        display.show(Image.BUTTERFLY)#仮
+
+
+    ### Timerモード(通常モード)処理
 
     #タイマー未動作状態の場合
     if appStatus == 0 :
         #点灯LED表示
-        display.show(dispTime.BlockArrey[blocks])
+        #display.show(dispTime.BlockArrey[blocks])
+        display.show(appStatus)
 
     #タイマー動作中状態の場合
     if appStatus == 1 :
-        #点灯LED表示
-        display.show(dispTime.BlockArrey[blocks] - 1)
-        #display.show(TwoLineNumber.img(25))
+        #残りブロック数取得
+        blocks = GetRemainingBlocks()
 
-        #点滅LED表示
+        #ブロックが0個になった場合
+        if blocks == 0 :
+            #満了演出
+            display.show(Image.HAPPY)
+
+            #状態初期化
+            appStatus = 0
+
+        #ブロックが残っている場合
+        else :
+            #点灯LED表示
+            display.show(dispTime.BlockArrey[blocks])
+            #display.show(appStatus)
+
+            #点滅LED表示
 
     #タイマー一時停止状態の場合
     if appStatus == 2 :
         break
 
-    # ボタンイベント登録
-    if button_a.is_pressed() and button_b.is_pressed():
-        break
-    elif button_a.is_pressed():
-        ButtonEvent.ButtonCkick_A(appStatus, blockNumber, blockSurvivalTime)
-    elif button_b.is_pressed():
-        ButtonEvent.ButtonCkick_B()
+
+
+
